8d8738d4319fd5bb17743d3faf716277fc589605
SetraR <setrar@valiha.com>
2020-05-12 15:33:11 -0400

    .

--- src/main/scala/com/eed3si9n/tetrix/Agent.scala
+++ src/main/scala/com/eed3si9n/tetrix/Agent.scala
@@ -1,6 +1,11 @@
 package com.eed3si9n.tetrix
 
 class Agent {
+
   def utility(state: GameState): Double =
     if (state.status == GameOver) -1000.0
-    else state.lineCount.toDouble}
+    else state.lineCount.toDouble
+
+  def bestMove(state: GameState): StageMessage = MoveLeft
+
+}
--- src/test/scala/AgentSpec.scala
+++ src/test/scala/AgentSpec.scala
@@ -8,6 +8,10 @@ class AgentSpec extends Specification with StateExample { def is =            s2
     evaluate initial state as 0.0,                                            $utility1
     evaluate GameOver as -1000.0,                                             $utility2
     evaluate an active state by lineCount                                     $utility3
+
+  Solver should
+    pick MoveLeft for s1                                                      $solver1
+    pick Drop for s3                                                          $solver2
                                                                               """
 
   import com.eed3si9n.tetrix._
@@ -23,4 +27,8 @@ class AgentSpec extends Specification with StateExample { def is =            s2
     val s = Function.chain(Nil padTo (19, tick))(s3)
     agent.utility(s) must_== 1.0
   }
+  def solver1 =
+    agent.bestMove(s1) must_== MoveLeft
+  def solver2 =
+    agent.bestMove(s3) must_== Drop
 }
